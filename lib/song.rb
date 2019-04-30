6621605fba9fc78532f3f17a1aa1c4f0a00c4548
Jim Hough <teachlife@outlook.com>
2019-04-30 23:47:51 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -42,9 +42,8 @@ class Song
   def self.find_or_create_by_name(name)
     if self.find_by_name(name) != false
       self.find_by_name(name)
-    else
-      self.create_by_name(name)
     end
+    self.create_by_name(name)
   end
 
 
